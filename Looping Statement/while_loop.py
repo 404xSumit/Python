f3f298c25a2a9388b1b72f7adc75f31883f48c3e
Sumit Singh Thakur <sumit942538@gmail.com>
2025-07-05 20:34:02 +0530

    Practices some question of  while loop

---
+++ Looping Statement/while_loop.py
@@ -0,0 +1,43 @@
+
+"""Separate each digit of a number and print it on the new line"""
+
+# n = int(input("Enter the number:- "))
+# a = len(str(n))
+# while a>0:
+#     rem = n%10
+#     print(rem)
+#     n=n//10
+
+
+""" Accept a number and print its reverse"""
+# n = int(input("Enter the number:- "))
+# add = 0 
+# while n > 0:
+#     add = add * 10 + (n%10)
+#     n = n // 10
+# print(add)
+
+
+"""Accept a number and check if it is a pallindromic number (If number and its reverse are equal)"""
+# n = int(input("Enter the number:- "))
+# add = 0
+# while n>0:
+#     add =( add * 10 ) + (n%10)
+#     n = n//10
+# if n == add:
+#     print("It is a palindromic syntax ")
+# else:
+#     print("It is not a palindromic syntax")
+
+"""Create a random number guessing game with python."""
+# import random
+
+# a = random.randint(1,15)
+# while a > 0:
+#     b = int(input("Enter the number here:- "))
+#     print("Guess the number between 1-15")
+#     if a == b :
+#         print("You guessed it right ")
+#         break
+#     else:
+#         print("TRY AGAIN")
